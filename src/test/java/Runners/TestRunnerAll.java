a30072f467847b0db19662e0f57f7d887b9a36b9
Nazım-PC <nzm.eryilmaz@gmail.com>
2022-09-17 20:21:13 +0300

    final commit

--- src/test/java/Runners/TestRunnerAll.java
+++ src/test/java/Runners/TestRunnerAll.java
@@ -4,8 +4,9 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
-        features = {"src/test/java/FeatureFiles/"},
-        glue = {"StepDefinitions"}
+        features = {"src/test/java/FeatureFiles/Login.feature"},
+        glue = {"StepDefinitions"},
+        plugin = { "html:target//cucumber-reports.html" }
 )
 public class TestRunnerAll extends AbstractTestNGCucumberTests {
 }
